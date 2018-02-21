db3ab88fe955235cb750284255d5c58b2b2cad66
Zuzanna Stolińska <zuzanna.stolinska@netguru.pl>
2018-02-21 21:23:56 +0100

    Add todo notes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  # TODO add database relations
+  # TODO add model validations
 end
