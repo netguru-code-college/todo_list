db4899ee3f3b185165e53c7c146f72d219b15e7b
Zuzanna Stolińska <zuzanna.stolinska@netguru.pl>
2018-02-21 15:49:05 +0100

    Add devise and User

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   get '/home', to: 'home#show'
-
+  devise_for :users
   root 'lists#index'
 
   resources :lists
+  resources :users, only: [:show, :index, :update, :delete]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
