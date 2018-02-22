f89bcee747b63bcf8a2762bdd590926055ae923f
Zuzanna Stolińska <zuzanna.stolinska@netguru.pl>
2018-02-22 11:19:19 +0100

    Add Bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
 
   resources :lists
   resources :users, only: [:show, :index, :edit, :update, :delete]
+
+  get '/sth', to: Proc.new { [200, {}, ['ok']] }
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
