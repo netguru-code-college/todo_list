61b905ee5d147e23a03300a6c05d1b5b64ef244b
Zuzanna Stolińska <zuzanna.stolinska@netguru.pl>
2018-02-22 14:15:38 +0100

    Handle empty names in full_name

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -2,10 +2,23 @@ class UserDecorator < ApplicationDecorator
   delegate_all
 
   def full_name
+    return object.email      if first_name_blank? && last_name_blank?
+    return object.first_name if last_name_blank?
+    return object.last_name  if first_name_blank?
     "#{object.first_name} #{object.last_name}"
   end
 
   def creation_date
     object.created_at.strftime('%A, %B %e')
+  end
+
+  private
+
+  def first_name_blank?
+    object.first_name.blank?
+  end
+
+  def last_name_blank?
+    object.last_name.blank?
   end
 end
--- spec/decorators/user_decorator_spec.rb
+++ spec/decorators/user_decorator_spec.rb
@@ -5,8 +5,34 @@ RSpec.describe UserDecorator do
   let(:decorated_user) { user.decorate }
 
   describe '#full_name' do
-    it 'returns full user name' do
-      expect(decorated_user.full_name).to eq 'Jane Snow'
+    context 'first and last name are present' do
+      it 'returns full user name' do
+        expect(decorated_user.full_name).to eq 'Jane Snow'
+      end
+    end
+
+    context 'last name is empty' do
+      before { user.update(last_name: nil) }
+
+      it 'returns only first name' do
+        expect(decorated_user.full_name).to eq 'Jane'
+      end
+    end
+
+    context 'first name is empty' do
+      before { user.update(first_name: nil) }
+
+      it 'returns only last name' do
+        expect(decorated_user.full_name).to eq 'Snow'
+      end
+    end
+
+    context 'first and last name are empty' do
+      before { user.update(first_name: nil, last_name: nil) }
+
+      it 'returns user email' do
+        expect(decorated_user.full_name).to eq user.email
+      end
     end
   end
 end
